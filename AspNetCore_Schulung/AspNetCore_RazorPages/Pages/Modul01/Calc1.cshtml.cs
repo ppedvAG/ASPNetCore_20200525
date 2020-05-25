04ded2c482edbf5b20e8021f9b4ed1a14361b2c8
Kevin Winter <KevinW@ppedv.de>
2020-05-25 13:17:57 +0200

    Erster Rundumblick Gemeinsamkeiten zwischen RazorPages und MVC
    Razor-Syntax Samples in RaorPages-Projekt

---
+++ AspNetCore_Schulung/AspNetCore_RazorPages/Pages/Modul01/Calc1.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspNetCore_RazorPages.Pages.Modul01
+{
+    public class Calc1Model : PageModel
+    {
+        public int Ergebnis { get; set; }
+
+        public void OnGet()
+        {
+            Ergebnis = 0;
+        }
+
+
+
+        //URL müsste lauten: 
+        public void OnGetHannes()
+        {
+            Ergebnis = 42;
+        }
+
+
+        //Bei einem einfachen Submit wird OnPost ausgeführt
+        public void OnPost()
+        {
+            int eins = int.Parse(Request.Form["eins"].FirstOrDefault());
+            int zwei = int.Parse(Request.Form["zwei"].FirstOrDefault());
+
+            Ergebnis = eins + zwei;
+        }
+
+    }
+}
